9cf5b3f5dda47e9c45fd86faae688b2ba2151e35
Kate <kasia@meiselfashion.com>
2022-11-05 15:11:44 +0100

    workshop1 - zadanie jest niedokończone, dodałam pytanie w jednym miejscu, mam też drugie pytanie o powiązanie z utworzoną klasą Actor

---
+++ jdbc/src/main/java/pl/sdacademy/java/jdbc/homework1/Actor.java
@@ -0,0 +1,12 @@
+package pl.sdacademy.java.jdbc.homework1;
+
+import java.util.List;
+
+public class Actor {
+
+    private String first_name;
+    private String last_name;
+    List<Actor> actors;
+
+
+}
